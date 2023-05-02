d55a46d4a9e8fa23aee809f380cc05528a1cfc3d
ihssane hanzaz <i.hanzaz@piman-group.fr>
2023-05-02 09:52:08 +0100

    chore: installed and configured angular firebase

---
+++ src/environments/environment.default.ts
@@ -0,0 +1,12 @@
+export const defaultEnvironment = {
+    firebase: {
+      apiKey: "AIzaSyBF_ex3p2btzuS6ZzD29F52NU6_7E3nTgk",
+      authDomain: "anywr-1b4d6.firebaseapp.com",
+      projectId: "anywr-1b4d6",
+      storageBucket: "anywr-1b4d6.appspot.com",
+      messagingSenderId: "917579097769",
+      appId: "1:917579097769:web:fbce8d58077888f4b2ebb8",
+      measurementId: "G-6F1YQBNH9V"
+    },
+    production: false
+};
