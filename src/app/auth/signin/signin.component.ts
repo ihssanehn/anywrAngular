2d696834bb824093f852ed5bb481afab9a3b0ecf
ihssane hanzaz <i.hanzaz@piman-group.fr>
2023-05-02 09:28:57 +0100

    feat: added module auth and compo sign in

---
+++ src/app/auth/signin/signin.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss']
+})
+export class SigninComponent implements OnInit{
+
+  form!: FormGroup;
+  isLoggingIn = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) { }
+  
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+
+  login() {
+    this.isLoggingIn = true;
+  }
+
+
+
+}
